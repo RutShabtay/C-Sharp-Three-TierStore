b29884187bb42127d59ade527e2a1364c1cecb76
RuthiShabtay <rs0548541650@gmail.com>
2025-04-24 14:26:41 +0300

    BL layer in construction processes

--- DotNet2025_7984_4327/BL/Blimplementation/OrderImplementation.cs
+++ DotNet2025_7984_4327/BL/Blimplementation/OrderImplementation.cs
@@ -1,4 +1,5 @@
 ﻿using BlApi;
+using BO;
 using DalApi;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,19 @@ using System.Threading.Tasks;
 
 namespace Blimplementation
 {
-    internal class OrderImplementation:IOrder
+    internal class OrderImplementation : IOrder
     {
         private IDal _dal = DalApi.Factory.Get;
+
+        List<SaleInProduct>? AddProductToOrder(Order order, int productId, int quantity);
+        void CalcTotalPriceForProduct(ProductInOrder product);
+        void CalcTotalPrice(Order order);
+        void DoOrder(Order order);
+        void SearchSaleForProduct(ProductInOrder product, bool isPreferredCustomer)
+        {
+            List<SaleInProduct>? tempSaleList = ProductImplementation.SalesInProduct(product.productId, isPreferredCustomer);
+            tempSaleList?.Select(x => (x.QuantityForSale <= product.quantityInOrder) && (x.isIntendedForAllCustomers || isPreferredCustomer)).ToList();
+        }
 
     }
 }
--- DotNet2025_7984_4327/BL/Blimplementation/ProductImplementation.cs
+++ DotNet2025_7984_4327/BL/Blimplementation/ProductImplementation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BO;
+using DalApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,92 @@ using System.Threading.Tasks;
 
 namespace Blimplementation
 {
-    internal class ProductImplementation
+    internal class ProductImplementation : BlApi.IProduct
     {
+        private IDal _dal = DalApi.Factory.Get;
+        public int Create(BO.Product item)
+        {
+            try
+            {
+                return _dal.Product.Create(item.ProductToDo());
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+        }
+        public BO.Product? Read(int id)
+        {
+            try
+            {
+                return _dal.Product.Read(id)?.ProductToBo();
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+
+        }
+        public BO.Product? Read(Func<DO.Product, bool> filter)
+        {
+            try
+            {
+                return _dal.Product.Read(filter)?.ProductToBo();
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+        }
+        public List<BO.Product> ReadAll(Func<DO.Product, bool>? filter = null)
+        {
+            try
+            {
+                return _dal.Product.ReadAll(filter)?.Select(q => q.ProductToBo()).ToList() ?? new List<BO.Product>();
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+        }
+        public void Update(BO.Product item)
+        {
+            try
+            {
+                _dal.Product.Update(item.ProductToDo());
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+        }
+        public void Delete(int id)
+        {
+            try
+            {
+                _dal.Product.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+        }
+
+        public void salesInProduct(int productId, bool isPreferredCustomer)
+        {
+            List<DO.Sale> allSales;
+            try
+            {
+                allSales = _dal.Sale.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                throw new BO.BlDeliverDalException(ex, ex.Message);
+            }
+
+            allSales.Select(s => s.productId == productId && s.saleStartDate <= DateTime.Today && s.saleEndDate >= DateTime.Today).ToList();
+            (allSales.Select(s => s.saleToSaleInProduct()).ToList() ?? new List<SaleInProduct>());
+
+        }
     }
 }
